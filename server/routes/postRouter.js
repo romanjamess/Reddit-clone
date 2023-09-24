db676972f91501fe4def8d52b18cf54a88ae3285
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-09-24 00:27:01 -0500

    Get post

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -1,37 +1,46 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import { prisma } from "../server.js";
-import bcrypt from "bcrypt";
 
 export const postRouter = express.Router();
 
-postRouter.get(`/`, async (req, res) => {
-    const posts = await prisma.post.findMany({
-        data: {
-            title: true,
-            text: true
-        }
-    })
-    res.send({
-        success: true,
-        posts
-    });
+postRouter.get("/", async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany({
+            include: {
+                user: true,
+                subreddit: true,
+                upvotes: true,
+                downvotes: true,
+            },
+        });
+        const data = {
+            success: true,
+            posts,
+        };
+        res.json({ data });
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message,
+        });
+    }
 });
 
-postRouter.post(`/`, async (req, res) => {
-    try { 
+
+postRouter.post("/", async (req, res) => {
+    try {
         const { title, text, subredditId } = req.body;
         const post = await prisma.post.create({
             data: {
-                title, 
+                title,
                 text,
                 userId: req.user.id,
                 subredditId
             }
-        })
+        });
         res.send({
             success: true,
-            post
+            post,
         });
     } catch (error) {
         res.send({
@@ -39,5 +48,4 @@ postRouter.post(`/`, async (req, res) => {
             error: error.message,
         });
     }
-
 });
