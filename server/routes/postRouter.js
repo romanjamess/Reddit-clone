587356ef7510800983b094d21285f42f7ad653bd
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-09-25 22:06:36 -0500

    put and delete routes for postRouter

--- server/routes/postRouter.js
+++ server/routes/postRouter.js
@@ -11,6 +11,7 @@ postRouter.get("/", async (req, res) => {
                 subreddit: true,
                 upvotes: true,
                 downvotes: true,
+                children: true
             },
         });
         const data = {
@@ -48,4 +49,65 @@ postRouter.post("/", async (req, res) => {
             error: error.message,
         });
     }
+});
+
+postRouter.put("/:postId", async (req, res) => {
+    try {
+        const { title, text, } = req.body;
+        const { postId } = req.params;
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: postId,
+                userId: req.user.id,
+            },
+        })
+
+        if (!post) {
+            return res.send({ success: false, error: "Post not found" });
+        }
+
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: postId,
+                userId: req.user.id,
+            },
+            data: {
+                title,
+                text,
+            },
+
+        })
+        res.send({ success: true, updatedPost })
+
+    } catch (error) {
+        res.send({ success: false, error: error.message })
+    }
+
+});
+
+postRouter.delete("/:postId", async (req, res) => {
+    try {
+        const { postId } = req.params;
+        const post = await prisma.post.findUnique({
+            where: {
+                id: postId,
+                userId: req.user.id,
+            },
+        })
+
+        if (!post) {
+            return res.send({ success: false, error: "Post not found" });
+        }
+        const deletedPost = await prisma.post.delete({
+            where: {
+                id: postId,
+            },
+        })
+        res.send({ success: true, deletedPost })
+
+    } catch (error) {
+        res.send({ success: false, error: error.message })
+    }
+
 });
