e2ace244c63f68f39f0956c57b3f74e371ed00bd
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-09-29 23:59:29 -0500

    user can now like

--- Reddit/src/components/newPost.jsx
+++ Reddit/src/components/newPost.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { useOutletContext, useNavigate } from "react-router-dom";
-import "../assets/post.css"; 
+import { useOutletContext } from "react-router-dom";
+import "../assets/post.css";
+import { BiDownvote, BiUpvote } from "react-icons/bi";
+import { API } from "../utils/index.js";
 
 const NewPost = () => {
-  const { post } = useOutletContext();
+  const { post, token , fetchPost} = useOutletContext();
+  
+  const handleUpvote = async (postId) => {
+    const response = await fetch(`${API}/votes/upvotes/${postId}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        postId: postId,
+      }),
+    });
+    const data = await response.json();
+    fetchPost();
+    console.log(data);
+  };
+
 
   return (
     <>
@@ -11,7 +30,27 @@ const NewPost = () => {
         {post.map((posts) => (
           <div className="reddit-post" key={posts.id}>
             <div className="post-header">
-              <p className="subreddit">{posts.subreddit.name}/{posts.user.username}</p>
+              <p className="subreddit">
+                {posts.subreddit.name}/ {posts.user.username}
+              </p>
+            </div>
+            <div className="display-likes">
+              <div className="like-buttons">
+                <div className="upVote">
+                  <button
+                    onClick={() => handleUpvote(posts.id)}
+                    className="icon-button"
+                  >
+                    <BiUpvote />
+                  </button>
+                </div>
+                <div className="downVote">
+                  <button className="icon-button">
+                    <BiDownvote />
+                  </button>
+                </div>
+              </div>
+              {posts.upvotes.length}
             </div>
             <h2 className="post-title">{posts.title}</h2>
             <p className="post-text">{posts.text}</p>
